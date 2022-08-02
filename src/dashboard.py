e7126e84fba48ca39592f0840e4e7ac0a91c486d
tukienhoa <tukienhoa1710@gmail.com>
2022-08-02 15:41:34 +0700

    Update XGBPredict

--- src/dashboard.py
+++ src/dashboard.py
@@ -26,48 +26,6 @@ df.index = df['Date']
 
 train = []
 valid = []
-#data = df.sort_index(ascending = True, axis = 0)
-# new_data = pd.DataFrame(index = range(0, len(df)), columns = ['Date', 'Close'])
-
-# for i in range(0, len(data)):
-#     new_data["Date"][i] = data['Date'][i]
-#     new_data["Close"][i] = data["Close"][i]
-    
-# new_data.index = new_data.Date
-# new_data.drop("Date", axis = 1, inplace = True)
-# dataset = new_data.values
-
-# train = dataset[0:987, :]
-# valid = dataset[987:, :]
-
-# scaler = MinMaxScaler(feature_range=(0,1))
-# scaled_data = scaler.fit_transform(dataset)
-
-# x_train, y_train = [], []
-
-# for i in range(60, len(train)):
-#     x_train.append(scaled_data[i - 60:i, 0])
-#     y_train.append(scaled_data[i, 0])
-
-
-# # Used for XGB
-# x_train_XGB = x_train
-# y_train_XGB = y_train
-
-# x_train, y_train = np.array(x_train), np.array(y_train)
-# x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-# inputs = new_data[len(new_data) - len(valid) - 60:].values
-# inputs = inputs.reshape(-1, 1)
-# inputs = scaler.transform(inputs)
-
-# X_test = []
-# for i in range(60,inputs.shape[0]):
-#     X_test.append(inputs[i - 60:i, 0])
-
-# X_test = np.array(X_test)
-# X_test_XGB = X_test
-# X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
 
 # App layout
@@ -95,8 +53,7 @@ app.layout = html.Div([
               ])
 def update_selected_pmethod(selected_method, selected_type):
     if (selected_method == "XGBoost"):
-        # [train, valid] = XGBPredict(x_train_XGB, y_train_XGB, X_test_XGB, new_data, scaler)
-        pass
+        [train, valid] = XGBPredict(df, selected_type)
     elif (selected_method == "RNN"):
         [train, valid] = RNNPredict(df, selected_type)
     else:
--- src/prediction_methods/xgboost_pred.py
+++ src/prediction_methods/xgboost_pred.py
@@ -1,34 +1,54 @@
 from xgboost import XGBRegressor
-# import math
 
-def XGBPredict(x_train, y_train, X_test, new_data, scaler):
-    # dataset = new_data.values
-    
-    # train_len = math.ceil(len(dataset) * 0.8)
-    # train = dataset[0:train_len, :]
-    # valid = dataset[train_len:, :]
+import pandas as pd
+pd.options.mode.chained_assignment = None
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
-    # x_train, y_train = [], []
+import math
 
-    # for i in range(60, len(train)):
-    #     x_train.append(dataset[i - 60:i, 0])
-    #     y_train.append(dataset[i, 0])
+def XGBPredict(df, type):
+    data = df.sort_index(ascending = True, axis = 0)
+    new_data = pd.DataFrame(index = range(0, len(df)), columns = ['Date', type])
 
-    # xgb = XGBRegressor()
-    # xgb.fit(x_train, y_train)
+    if (type == "Close"):
+        for i in range(0, len(data)):
+            new_data["Date"][i] = data['Date'][i]
+            new_data[type][i] = data[type][i]
+    elif (type == "Price of change"):
+        for i in range(0, len(data)):
+            new_data["Date"][i] = data['Date'][i]
+            if (i == 0):
+                new_data[type][i] = 0
+            else:
+                new_data[type][i] = (data["Close"][i] - data["Close"][i - 1]) / data["Close"][i - 1] * 100
+        
+    new_data.index = new_data.Date
+    new_data.drop("Date", axis = 1, inplace = True)
+    dataset = new_data.values
 
-    # inputs = new_data[len(new_data) - len(valid) - 60:].values
-    # inputs = inputs.reshape(-1, 1)
+    train_len = math.ceil(len(dataset) * 0.8)
+    train = dataset[0:train_len, :]
+    valid = dataset[train_len:, :]
 
-    # X_test = []
-    # for i in range(60,inputs.shape[0]):
-    #     X_test.append(inputs[i - 60:i, 0])
+    scaler = MinMaxScaler(feature_range=(0,1))
+    scaled_data = scaler.fit_transform(dataset)
 
-    # predictions = xgb.predict(X_test)
+    x_train, y_train = [], []
 
-    # train = new_data[:train_len]
-    # valid = new_data[train_len:]
-    # valid['Predictions'] = predictions
+    for i in range(60, len(train)):
+        x_train.append(scaled_data[i - 60:i, 0])
+        y_train.append(scaled_data[i, 0])
+
+    inputs = new_data[len(new_data) - len(valid) - 60:].values
+    inputs = inputs.reshape(-1, 1)
+    inputs = scaler.transform(inputs)
+
+    X_test = []
+    for i in range(60,inputs.shape[0]):
+        X_test.append(inputs[i - 60:i, 0])
+
+    X_test = np.array(X_test)
 
     model = XGBRegressor()
     model.fit(x_train, y_train)
@@ -37,8 +57,8 @@ def XGBPredict(x_train, y_train, X_test, new_data, scaler):
     predictions = scaler.inverse_transform(predictions.reshape(1, -1))
     predictions = predictions[0]
 
-    train = new_data[:987]
-    valid = new_data[987:]
+    train = new_data[:train_len]
+    valid = new_data[train_len:]
     valid['Predictions'] = predictions
 
     return [train, valid]
